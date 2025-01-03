037b7c868722fbecd3c247e88f9da631d47fd8ce
Abkadar Chaudhari <abdulkadir4self@gmail.com>
2025-01-03 11:35:10 +0530

    added bhadohi db

--- src/utils/pool.js
+++ src/utils/pool.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: '127.0.0.1', // Replace with your MySQL host
-  user: 'root',      // Replace with your MySQL username
-  password: '373920', // Replace with your MySQL password
-  database: 'bida_db', // Replace with your database name
+  host: '43.204.96.202', // Replace with your MySQL host
+  user: 'dev_bhadohi_user',      // Replace with your MySQL username
+  password: 'Bhadohi@321##$$', // Replace with your MySQL password
+  database: 'dev_bhadohi_property', // Replace with your database name
   waitForConnections: true, // Wait if no connections are available
   connectionLimit: 10,      // Number of connections in the pool
   queueLimit: 0,            // No limit on the number of requests in the queue
